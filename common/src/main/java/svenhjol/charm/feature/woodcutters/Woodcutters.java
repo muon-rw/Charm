493bec5a9c6b92b7fa24d4048b670e48ac3fd0b3
Sven H <sven.hjol@protonmail.com>
2023-01-25 17:26:14 +0000

    Migrate lumberjacks

--- common/src/main/java/svenhjol/charm/feature/woodcutters/Woodcutters.java
+++ common/src/main/java/svenhjol/charm/feature/woodcutters/Woodcutters.java
@@ -1,5 +1,6 @@
 package svenhjol.charm.feature.woodcutters;
 
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.inventory.MenuType;
@@ -8,7 +9,6 @@ import svenhjol.charm.Charm;
 import svenhjol.charm_core.annotation.Feature;
 import svenhjol.charm_core.base.CharmFeature;
 
-import java.util.Set;
 import java.util.function.Supplier;
 
 @Feature(mod = Charm.MOD_ID, priority = 1, description = "A functional block that adds more efficient recipes for crafting wooden stairs and slabs.")
@@ -34,7 +34,7 @@ public class Woodcutters extends CharmFeature {
             () -> new WoodcutterBlock.BlockItem(this, BLOCK));
 
         POI_TYPE = Charm.REGISTRY.pointOfInterestType(BLOCK_ID,
-            () -> new PoiType(Set.of(BLOCK.get().defaultBlockState()), 1, 1));
+            () -> new PoiType(ImmutableSet.copyOf(BLOCK.get().getStateDefinition().getPossibleStates()), 1, 1));
 
         RECIPE_BOOK_TYPE = Charm.REGISTRY.recipeBookType(BLOCK_ID);
         MENU = Charm.REGISTRY.menuType(BLOCK_ID,
