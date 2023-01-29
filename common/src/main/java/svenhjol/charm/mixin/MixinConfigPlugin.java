cd46890c6310d2de08d233d515669417c675331e
Sven H <sven.hjol@protonmail.com>
2023-01-29 20:43:28 +0000

    Use charm not charm client

--- common/src/main/java/svenhjol/charm/mixin/MixinConfigPlugin.java
+++ common/src/main/java/svenhjol/charm/mixin/MixinConfigPlugin.java
@@ -1,11 +1,11 @@
 package svenhjol.charm.mixin;
 
-import svenhjol.charm.CharmClient;
+import svenhjol.charm.Charm;
 import svenhjol.charm_core.base.BaseMixinConfigPlugin;
 
 public class MixinConfigPlugin extends BaseMixinConfigPlugin {
     @Override
     protected String getModId() {
-        return CharmClient.MOD_ID;
+        return Charm.MOD_ID;
     }
 }
