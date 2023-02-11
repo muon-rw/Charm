e330c322e81062510e35c9cacfdd15cb472dd21f
Sven H <sven.hjol@protonmail.com>
2023-02-11 16:00:20 +0000

    Fix crafting table cache stack overflow

--- common/src/main/java/svenhjol/charm/feature/portable_crafting/PortableCrafting.java
+++ common/src/main/java/svenhjol/charm/feature/portable_crafting/PortableCrafting.java
@@ -48,7 +48,7 @@ public class PortableCrafting extends CharmFeature implements IChecksInventoryTa
                 .flatMap(s -> s.getInventoryTagChecks().stream())
                 .anyMatch(check -> check.test(player, CRAFTING_TABLES));
 
-            if (hasCraftingTable) {
+            if (hasCraftingTable && !CACHED_HAS_CRAFTING_TABLE.contains(uuid)) {
                 CACHED_HAS_CRAFTING_TABLE.add(uuid);
             } else {
                 CACHED_HAS_CRAFTING_TABLE.remove(uuid);
