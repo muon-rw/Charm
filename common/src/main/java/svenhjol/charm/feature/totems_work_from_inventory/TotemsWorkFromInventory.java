9d6d8b535ac6006e48d8d065834b9bdc3f7cb7b7
Sven H <sven.hjol@protonmail.com>
2023-02-16 21:57:01 +0000

    Use new GetInventoryItem API

--- common/src/main/java/svenhjol/charm/feature/totems_work_from_inventory/TotemsWorkFromInventory.java
+++ common/src/main/java/svenhjol/charm/feature/totems_work_from_inventory/TotemsWorkFromInventory.java
@@ -2,8 +2,10 @@ package svenhjol.charm.feature.totems_work_from_inventory;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import svenhjol.charm.Charm;
 import svenhjol.charm_api.iface.IGetsInventoryItem;
 import svenhjol.charm_core.annotation.Feature;
@@ -11,7 +13,7 @@ import svenhjol.charm_core.base.CharmFeature;
 import svenhjol.charm_core.init.CharmApi;
 
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 @Feature(mod = Charm.MOD_ID, description = "A totem will work from anywhere in the player's inventory as well as held in the main or offhand.")
 public class TotemsWorkFromInventory extends CharmFeature implements IGetsInventoryItem {
@@ -26,8 +28,8 @@ public class TotemsWorkFromInventory extends CharmFeature implements IGetsInvent
             var checks = CharmApi.getProviderData(IGetsInventoryItem.class,
                 provider -> provider.getInventoryItem().stream());
 
-            for (Function<Player, ItemStack> check : checks) {
-                var result = check.apply(player);
+            for (var check : checks) {
+                var result = check.apply(player, Items.TOTEM_OF_UNDYING);
                 if (!result.isEmpty()) {
                     return result;
                 }
@@ -38,24 +40,24 @@ public class TotemsWorkFromInventory extends CharmFeature implements IGetsInvent
     }
 
     @Override
-    public List<Function<Player, ItemStack>> getInventoryItem() {
+    public List<BiFunction<Player, ItemLike, ItemStack>> getInventoryItem() {
         return List.of(
             // Main hand check.
-            player -> {
+            (player, item) -> {
                 var stack = player.getMainHandItem();
-                return stack.is(Items.TOTEM_OF_UNDYING) ? stack : ItemStack.EMPTY;
+                return stack.is((Item) item) ? stack : ItemStack.EMPTY;
             },
 
             // Off-hand check.
-            player -> {
+            (player, item) -> {
                 var stack = player.getOffhandItem();
-                return stack.is(Items.TOTEM_OF_UNDYING) ? stack : ItemStack.EMPTY;
+                return stack.is((Item) item) ? stack : ItemStack.EMPTY;
             },
 
             // Main inventory check.
-            player -> {
+            (player, item) -> {
                 var inventory = player.getInventory();
-                var slotMatchingItem = inventory.findSlotMatchingItem(new ItemStack(Items.TOTEM_OF_UNDYING));
+                var slotMatchingItem = inventory.findSlotMatchingItem(new ItemStack(item));
 
                 if (slotMatchingItem > 0) {
                     return inventory.getItem(slotMatchingItem);
