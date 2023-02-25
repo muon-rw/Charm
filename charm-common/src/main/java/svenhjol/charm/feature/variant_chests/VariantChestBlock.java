8cbfa7d6f281a28704f7d596d7606c37631144ad
Sven H <sven.hjol@protonmail.com>
2023-02-25 22:31:33 +0000

    Change access to VariantChestBlock blockitem

--- charm-common/src/main/java/svenhjol/charm/feature/variant_chests/VariantChestBlock.java
+++ charm-common/src/main/java/svenhjol/charm/feature/variant_chests/VariantChestBlock.java
@@ -37,7 +37,7 @@ public class VariantChestBlock extends ChestBlock implements IVariantChest {
         return Charm.LOADER.get(VariantChests.class).orElseThrow();
     }
 
-    static class BlockItem extends CharmBlockItem implements IFuelProvider {
+    public static class BlockItem extends CharmBlockItem implements IFuelProvider {
         private final IVariantMaterial material;
 
         public BlockItem(Supplier<VariantChestBlock> block) {
