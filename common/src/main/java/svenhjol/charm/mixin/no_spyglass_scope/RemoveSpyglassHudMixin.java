fd49fa0026fa5ed6e2765760e9a11326339b1fdb
Sven H <sven.hjol@protonmail.com>
2023-03-19 21:47:51 +0000

    Fix up for 1.19.4

--- common/src/main/java/svenhjol/charm/mixin/no_spyglass_scope/RemoveSpyglassHudMixin.java
+++ common/src/main/java/svenhjol/charm/mixin/no_spyglass_scope/RemoveSpyglassHudMixin.java
@@ -1,5 +1,6 @@
 package svenhjol.charm.mixin.no_spyglass_scope;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,9 +10,10 @@ import svenhjol.charm.feature.no_spyglass_scope.NoSpyglassScope;
 
 @Mixin(Gui.class)
 public abstract class RemoveSpyglassHudMixin {
-    @Shadow protected abstract void renderSpyglassOverlay(float scale);
 
     @Shadow private float scopeScale;
+
+    @Shadow protected abstract void renderSpyglassOverlay(PoseStack poseStack, float scopeScale);
 
     /**
      * Defer to shouldRemoveHud. If the check is false, render as normal.
@@ -20,12 +22,12 @@ public abstract class RemoveSpyglassHudMixin {
         method = "render",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/Gui;renderSpyglassOverlay(F)V"
+            target = "Lnet/minecraft/client/gui/Gui;renderSpyglassOverlay(Lcom/mojang/blaze3d/vertex/PoseStack;F)V"
         )
     )
-    public void hookRender(Gui inGameHud, float f) {
+    public void hookRender(Gui instance, PoseStack poseStack, float v) {
         if (!NoSpyglassScope.shouldRemoveHud()) {
-            this.renderSpyglassOverlay(this.scopeScale);
+            this.renderSpyglassOverlay(poseStack, this.scopeScale);
         }
     }
 }
