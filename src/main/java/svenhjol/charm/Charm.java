0b6bae94ba8be51693659af053a4691c6b5a253e
Sven H <sven.hjol@protonmail.com>
2023-08-31 18:28:19 +0000

    Remove redundant init

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -1,15 +1,8 @@
 package svenhjol.charm;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Charm.MOD_ID)
 public class Charm {
     public static final String MOD_ID = "charm";
-
-    public Charm() {
-        // There is no common code so just execute the client.
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> CharmClient::new);
-    }
 }
