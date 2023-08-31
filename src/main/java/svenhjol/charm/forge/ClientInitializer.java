159840ed24548f877dec85a0dc41059ecdb1c243
Sven H <sven.hjol@protonmail.com>
2023-08-31 23:34:54 +0000

    Init steps to client initializer

--- src/main/java/svenhjol/charm/forge/ClientInitializer.java
+++ src/main/java/svenhjol/charm/forge/ClientInitializer.java
@@ -1,7 +1,14 @@
 package svenhjol.charm.forge;
 
+import svenhjol.charm.CharmClient;
+import svenhjol.charm_core.annotation.ClientFeature;
+
 public class ClientInitializer {
     public ClientInitializer() {
+        var client = CharmClient.instance();
+        var loader = client.loader();
 
+        // Autoload all annotated client features from the feature namespace.
+        loader.init(client.featurePrefix(), ClientFeature.class);
     }
 }
