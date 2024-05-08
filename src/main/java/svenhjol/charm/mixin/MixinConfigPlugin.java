d6cd57b5787f4b42ffc4ee735124f25f90a2a524
muon-rw <pashah012@gmail.com>
2024-05-08 12:07:11 -0500

    Automatically disable more features

--- src/main/java/svenhjol/charm/mixin/MixinConfigPlugin.java
+++ src/main/java/svenhjol/charm/mixin/MixinConfigPlugin.java
@@ -1,6 +1,7 @@
 package svenhjol.charm.mixin;
 
 import svenhjol.charm.Charm;
+import svenhjol.charm.feature.extractable_enchantments.ExtractableEnchantments;
 import svenhjol.charmony.base.CharmonyMixinConfigPlugin;
 import svenhjol.charmony.helper.ConfigHelper;
 
@@ -18,7 +19,22 @@ public class MixinConfigPlugin extends CharmonyMixinConfigPlugin {
         return List.of(
             feature -> feature.equals("ColoredGlintSmithingTemplates") && ConfigHelper.isModLoaded("optifabric"),
             feature -> feature.equals("NoChatUnverifiedMessage") && ConfigHelper.isModLoaded("chatsigninghider"),
-            feature -> feature.equals("ExtraStackables") && ConfigHelper.isModLoaded("allstackable")
+            feature -> feature.equals("ExtraStackables") && ConfigHelper.isModLoaded("allstackable"),
+            feature -> feature.equals("Lumberjacks")
+                    || feature.equals("Woodcutters")
+                    || feature.equals("Woodcutting")
+                    && ConfigHelper.isModLoaded("sawmill"),
+            feature -> feature.equals("UnlimitedRepairCost")
+                    || feature.equals("ShowRepairCost")
+                    || feature.equals("StrongerAnvils")
+                    || feature.equals("AerialAffinity")
+                    || feature.equals("AerialAffinityEnchantment")
+                    && ConfigHelper.isModLoaded("zenith"),
+            feature -> feature.equals("AutoRestock")
+                    || feature.equals("InventoryTidying")
+                    && ConfigHelper.isModLoaded("inventoryprofilesnext"),
+            feature -> feature.equals("NoExperimentalScreen")
+                    && ConfigHelper.isModLoaded("yeetusexperimentus")
         );
     }
 }
