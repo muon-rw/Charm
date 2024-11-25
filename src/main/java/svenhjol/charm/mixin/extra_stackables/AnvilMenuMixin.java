31abf04af755872423f6299042fc257404e61ff9
muon-rw <pashah012@gmail.com>
2024-11-25 16:23:00 -0600

    Clean up stackable book combining

--- src/main/java/svenhjol/charm/mixin/extra_stackables/AnvilMenuMixin.java
+++ src/main/java/svenhjol/charm/mixin/extra_stackables/AnvilMenuMixin.java
@@ -1,40 +1,43 @@
 package svenhjol.charm.mixin.extra_stackables;
 
+
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.Container;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * Changes the behavior of stacked enchanted books to only apply one book at a time
- * Mostly done to prevent cost-capping exploits with DragonLoot
- * Mixin priority is shifted slightly later due to a conflicting mixin from Charmony.
- */
-@Mixin(value = AnvilMenu.class, priority = 1100)
+@Mixin(AnvilMenu.class)
 public abstract class AnvilMenuMixin {
     @Shadow
     public int repairItemCountCost;
 
+    @Unique
     private boolean leftStackEnchantedBooks() {
-        ItemStack leftSlot = ((AnvilMenu)(Object)this).inputSlots.getItem(0);
-        return leftSlot.getItem() == Items.ENCHANTED_BOOK && leftSlot.getCount() > 1;
+        ItemStack leftSlot = ((AnvilMenu) (Object) this).getSlot(AnvilMenu.INPUT_SLOT).getItem();
+        return leftSlot.is(Items.ENCHANTED_BOOK) && leftSlot.getCount() > 1;
     }
+
+    @Unique
     private boolean rightStackEnchantedBooks() {
-        ItemStack rightSlot = ((AnvilMenu)(Object)this).inputSlots.getItem(1);
-        return rightSlot.getItem() == Items.ENCHANTED_BOOK && rightSlot.getCount() > 1;
+        ItemStack rightSlot = ((AnvilMenu) (Object) this).getSlot(AnvilMenu.ADDITIONAL_SLOT).getItem();
+        return rightSlot.is(Items.ENCHANTED_BOOK) && rightSlot.getCount() > 1;
     }
+
 
     /**
      * Always calculate the level cost as if there is only 1 item in the left stack
-     *
+     * <p>
      * Why is it hard coded to cost 40 levels if the stack size is over 1?
      * The world will never know
      */
@@ -43,62 +46,48 @@ public abstract class AnvilMenuMixin {
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/item/ItemStack;getCount()I",
-                    ordinal = 0
-            ),
-            slice = @Slice(
-                    from = @At(
-                            value = "INVOKE",
-                            target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"
-                    ),
-                    to = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/inventory/AnvilMenu;broadcastChanges()V"
-                    )
+                    ordinal = 1
             )
     )
     private int fakeInputStackSize(int original) {
-        if (leftStackEnchantedBooks()) {
-            return 1;
-        }
-        return original;
+        return leftStackEnchantedBooks() ? 1 : original;
     }
 
     /**
      * Change the output to always be 1 book at a time
      */
-    @Redirect(
+    @WrapOperation(
             method = "createResult",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/inventory/ResultContainer;setItem(ILnet/minecraft/world/item/ItemStack;)V"
             )
     )
-    private void capResultSlot(ResultContainer resultSlots, int slot, ItemStack stack) {
+    private void capResultSlot(ResultContainer instance, int i, ItemStack itemStack, Operation<Void> original) {
         if (leftStackEnchantedBooks()) {
-            stack.setCount(1);
+            itemStack.setCount(1);
         }
-        resultSlots.setItem(slot, stack);
+        original.call(instance, i, itemStack);
     }
+
 
     /**
      * Make createResult think we only have 1 book in the right slot -
-     *
+     * <p>
      * Causes the right slot shrink by 1 instead of clearing
      * without needing to mixin to multiple setItems or set temporary flags
-     *
+     * <p>
      * Also prevents XP cost from increasing when multiple books are
      * placed into the right stack.
-     *
      */
-    @Redirect(
+    @WrapOperation(
             method = "createResult",
             at = @At(
                     value = "FIELD",
-                    target = "Lnet/minecraft/world/inventory/AnvilMenu;repairItemCountCost:I",
-                    opcode = Opcodes.PUTFIELD
+                    target = "Lnet/minecraft/world/inventory/AnvilMenu;repairItemCountCost:I"
             )
     )
-    private void capRepairItemCountCost(AnvilMenu instance, int value) {
+    private void capRepairItemCountCost(AnvilMenu instance, int value, Operation<Void> original) {
         if (rightStackEnchantedBooks()) {
             repairItemCountCost = 1;
         } else {
@@ -109,7 +98,7 @@ public abstract class AnvilMenuMixin {
     /**
      * Shrink the left slot by 1 instead of clearing it
      */
-    @Redirect(
+    @WrapOperation(
             method = "onTake",
             at = @At(
                     value = "INVOKE",
@@ -117,14 +106,19 @@ public abstract class AnvilMenuMixin {
                     ordinal = 0
             )
     )
-    private void shrinkLeftSlot(Container inv, int index, ItemStack stack) {
+    private void shrinkLeftSlot(Container instance, int i, ItemStack itemStack, Operation<Void> original) {
         if (leftStackEnchantedBooks()) {
-            ItemStack leftSlot = inv.getItem(index);
+            ItemStack leftSlot = instance.getItem(i);
             leftSlot.shrink(1);
-            inv.setItem(index, leftSlot.isEmpty() ? ItemStack.EMPTY : leftSlot);
+            instance.setItem(i, leftSlot.isEmpty() ? ItemStack.EMPTY : leftSlot);
         } else {
-            inv.setItem(index, stack);
+            original.call(instance, i, itemStack);
         }
     }
 
+    @Inject(method = "onTake", at = @At("TAIL"), require = 1)
+    private void syncAfterTake(CallbackInfo ci) {
+        ((AnvilMenu)(Object)this).createResult();
+        ((AbstractContainerMenu)(Object)this).broadcastChanges();
+    }
 }
